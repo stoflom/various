3d523fc01483b2b4e6bd5eb111472f25e9b32cb0
Christoffel Lombard <stoflom@gmail.com>
2025-10-29 21:31:18 +0200

    cleanup

--- xmlgpx.pl
+++ xmlgpx.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
-# Reads a GPX XML file and extract <trk><name> elements using XML::LibXML with XPath and namespaces.
-# If the file contains <trk> elements the <name> of each track is printed else nothing is printed.
+# Reads a GPX XML file created by gpsbabel and extracts <trk><name> elements using XML::LibXML with XPath and namespaces.
+# If the file contains <trk><name> the name of each track is printed else nothing is printed.
 
 # use strict;
 use warnings;
@@ -20,7 +20,6 @@ $xpc->registerNs('tx', "http://www.topografix.com/GPX/1/0");
 
 
 # Find nodes using XPath
-my ($tx_trk_name) = $xpc->findnodes('//tx:trk/tx:name');
 
 foreach my $node ($xpc->findnodes('//tx:trk/tx:name')) {
     print "TRK-NAME: " . $node->textContent  . "\n" ;
